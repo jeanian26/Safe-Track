c832bbdede554326d3d7cc160d1a790edaf2e63a
Paul Ian <paulian829@gmail.com>
2021-10-15 22:44:15 +0800

    Add Camera function

--- android/app/src/main/java/com/foodvila/MainApplication.java
+++ android/app/src/main/java/com/foodvila/MainApplication.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
+import com.rnfs.RNFSPackage;
+import org.reactnative.camera.RNCameraPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -11,6 +15,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.RNFetchBlob.RNFetchBlobPackage; 
 
 public class MainApplication extends Application implements ReactApplication {
 
